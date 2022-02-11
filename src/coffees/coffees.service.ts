775654ec8f28fb15677912dcb072739d32ae7938
KauztoKuro <ikkikurogane70@gmail.com>
2022-02-11 11:36:29 +0630

    L:15 - Encompass BusinessDomain in Modules

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -17,7 +17,7 @@ export class CoffeesService {
   }
 
   findOne(id: string) {
-      throw 'A random error';
+    //   throw 'A random error';
       const coffee = this.coffees.find(item => item.id ===+id);
       if(!coffee) {
           throw new NotFoundException(`Coffee #${id} not found`);
