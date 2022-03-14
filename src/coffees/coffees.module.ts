edbf2e25f5682ba74520b162ad32790824b07660
KauztoKuro <ikkikurogane70@gmail.com>
2022-03-15 03:16:53 +0630

    L-38: class providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -7,12 +7,22 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
-class MockCoffeeService {}
+// class MockCoffeeService {}
+class ConfigService {}
+class DevelopmentconfigService {}
+class ProductConfigService {}
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentconfigService
+          : ProductConfigService,
+    },
     { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
     // {
     // provide: CoffeesService,
